2f5f845147b33092f852ac45fcc697b17cb84f09
abiaosita-Geraldo <abiaosita@gmail.com>
2026-09-07 02:57:04 +0100

    Update sales_qeuries.sql

--- sales_qeuries.sql
+++ sales_qeuries.sql
@@ -11,26 +11,26 @@ SELECT AVG(quantity) AS average_quantity_sold
 FROM salesdata_2024;
 
 SELECT min(quantity)
-FROM salesdata_2024
+FROM salesdata_2024;
 
 
 SELECT max(quantity)
-FROM salesdata_2024
+FROM salesdata_2024;
 
 SELECT top(2)  category
 FROM salesdata_2024
 where region = 'north'
 
 SELECT sum(revenue) as Total_Revenue
-FROM salesdata_2024
+FROM salesdata_2024;
 
 SELECT category, sum(revenue) as category_revenue
 FROM salesdata_2024
 GROUP BY category
 ORDER BY category_revenue  desc
  
-SELECT COUNT(*) customer_id
-FROM salesdata_2024
+SELECT COUNT(*) ,customer_id
+FROM salesdata_2024;
 
 
 SELECT category, sum(quantity) as sales
@@ -45,12 +45,12 @@ WHERE  category in ('electronics','clothing')
 GROUP BY category
 
 SELECT * ,sum (quantity) over(partition by category) as total_sales
-FROM salesdata_2024
+FROM salesdata_2024;
 
 
 SELECT * ,rank() over(order by quantity desc) as descending,
 rank()over(order by quantity asc) as ascending
-FROM salesdata_2024
+FROM salesdata_2024;
  
 
 ;WITH CTE As(
@@ -69,7 +69,7 @@ WHERE descending <=2 OR  ascending <=2;
 
 SELECT *
 FROM SALESDATA_2024
-FROM region IN ( 'west' ,'north')
+FROM region IN ( 'west' ,'north');
 
 
 SELECT *
